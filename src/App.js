bdad2f9e04ec2215f46d73e83570e4b4e4210ed7
moxco2008 <42118803+moxco2008@users.noreply.github.com>
2024-02-26 13:09:28 +0100

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Navbar from './Navbar.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar/>
+       <div className="content">
+        <h1>Welcome to ASD Unity Bank</h1>
+       </div>
+    </div>
+  );
+}
+
+export default App;
